e45c9bd8350edfd83e7d834c2e88d96d1feebfe1
Alfa Indica <indica464@gmail.com>
2025-08-04 14:11:22 +0700

    learn middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set("view engine", "ejs");
+
+// third party middleware
 app.use(expressEjsLayouts);
+app.use(morgan(process.env.NODE_ENV == "production" ? "tiny" : "dev"));
+
+// built in middleware
+app.use(express.static("public"));
+
+// application level middleware
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
 
 app.get("/", (req, res) => {
   const mahasiswa = [
